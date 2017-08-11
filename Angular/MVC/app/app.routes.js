b43a7f22f7acdf8a8114addad1c4b186899e9245
tyrocoders <zach@tyrocoders.com>
2017-08-10 19:47:35 -0600

    Add files via upload

---
+++ Angular/MVC/app/app.routes.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict'
+
+  angular
+      .module('app')
+      .config(config)
+
+  config.$inject = ['$stateProvider'];
+
+  function config($stateProvider) {
+      $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/authentication/login.tpl.html',
+            controller: 'LoginCtrl'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'app/authentication/register.tpl.html',
+            controller: 'RegisterCtrl'
+        });
+  } 
+})();
